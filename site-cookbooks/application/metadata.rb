15c28fb1c47268bb335157c04a3812e151afb501
Timur Vafin <timur.vafin@flatstack.com>
2013-04-15 17:46:26 +0400

    Move application recipes to site-cookbook

---
+++ site-cookbooks/application/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       'FlatStack'
+maintainer_email 'support@flatstack.com'
+license          'All rights reserved'
+description      'Installs/Configures Rails3 Base application'
+long_description 'Installs/Configures Rails3 Base application'
+version          '0.0.1'
+
+depends 'flatstack'
